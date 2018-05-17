0c8a2c5d31146f2b2b4ee78aa6e50c68baf1464b
greg-han <ghan3@mail.ccsf.edu>
2018-05-17 16:11:03 -0700

    only worked for few hours today. Now have basic functionality. Need to add win states, and then program the AI and the game will be complete (hopefully)

--- js/index.js
+++ js/index.js
@@ -6,13 +6,17 @@ var selectorX = document.getElementById("X");
 var selectorO = document.getElementById("O");
 
 //Framework for the game.
+//previous assignment should not be turn.
 var turn = "";
+var prevAssignment = "";
 var BOARD_LENGTH = 9;
 var SOLUTION_LENGTH = 7;
 var inPlay = false;
+var asked = false;
 var board = [11,12,13,21,22,23,31,32,33];
+var ORIGboard = [11,12,13,21,22,23,31,32,33];
 var ORIGsolutions = [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33]];
-var ORIGboard = {
+var ORIGneighbors = {
  "11" : [11,12,13,21,31,22,33],
  "12" : [11,13,21,22,23],
  "13" : [12,11,23,33,22,31],
@@ -23,26 +27,39 @@ var ORIGboard = {
  "32" : [22,12,31,33,21,23],
  "33" : [32,31,23,13,22,11]
  }
-var parseMap ={
- "11" : oneone, "12" : onetwo, "13":onethree,
- "21" : twoone, "22" : twotwo, "23": twothree,
- "31" : threeone, "32" : threetwo, "33" :threethree
- }
+
 //End framework
 
 //All variables that will have values upon construction
 var human;
 var ai;
-var boardConstraints;
+var boardConstraints; 
 var oneone,onetwo,onethree,twoone,twotwo,twothree,threeone,threetwo,threethree;
+var parseMap = {
+ '11' : 'oneone' , '12' : 'onetwo', '13':'onethree',
+ '21' : 'twoone', '22' : 'twotwo', '23': 'twothree',
+ '31' : 'threeone', '32' : 'threetwo', '33' : 'threethree'
+ }
+
 //end constructor variables.
 
 //player object.
-var player = function(){
-  var side = "";
-  var solved = false;
-  //check if you are close to a solution state by seeing if any have one left.
-  var solutionStates = [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33]];
+//check if you are close to a solution state by seeing if any have one left.
+/*var player = function(){
+  self.side = "";
+  self.solved = false;
+  self.solutionStates = [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33]];
+}*/
+var human = {
+  'side' : "",
+  'solved' : false,
+  'solutionStates' : [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33]]
+}
+
+var ai = {
+  'side' : "",
+  'solved' : false,
+  'solutionStates' : [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33]]
 }
 
 //constructor
@@ -57,9 +74,8 @@ function init(){
  threeone = document.getElementById("31");
  threetwo = document.getElementById("32");
  threethree = document.getElementById("33");
- human = new player();
- ai = new player();
- boardConstraints = ORIGboard;
+ boardConstraints = ORIGneighbors;
+ 
 }
 
 //Simply inputs the square, no smarts.
@@ -67,45 +83,51 @@ function init(){
 //however, you would make this the onclick function so you would have conditional checks in between.
 function fillSquare(square){
  if (inPlay){
-  var csquarenum = parseInt(parseMap(String(square).innerHTML == human.side));
-  for(var i = 0; i < BOARD_LENGTH; i++){
+  var squarenum = parseInt(square);
     //This already checks the constraints.
-    if(board.indexOf(squarenum) != NaN){
-     //constraint filling function
-     updateConstraints(squarenum); 
-     //update solution states
      //update board.
      if(turn == "human"){
        ///do consistency checks here, do not change turns until
-       //a square has been picked. You can 
-       board[board.indexOf(squarenum)] = String(human.side);
+       //a square has been picked. You can
+        if(board.indexOf(squarenum) != -1 && prevAssignment != eval(turn).side){
+         board[board.indexOf(squarenum)] = String(human.side);
+         eval(parseMap[String(square)]).innerHTML = human.side;
+         prevAssignment = human.side;
+         changeTurn();
+        }
+
      }//end of if turn is human
      else{//if turn is ai.
-       board[board.indexOf(squarenum)] = String(ai.side);
+        if(board.indexOf(squarenum) != -1 && prevAssignment != eval(turn).side){
+         board[board.indexOf(squarenum)] = String(ai.side);
+         eval(parseMap[String(square)]).innerHTML = ai.side;
+         prevAssignment = ai.side;
+         changeTurn();
+       }//end of if the board is not already filled.
      }//end of else ai
-    }//end of checking if the part of the board has this value "free" (Nan)
-   }//end of the for loop going through the board array.
- }// end of inPlay
- changeTurn();
-}// end of fillsqurae function
+   }//end of inplay
+  //constraint filling function
+  updateConstraints(squarenum); 
+  //update1solution states
+  updateSolutionStates(squarenum);
+ }//end of fillsquare 
 
 
 //This will raise the letters of the selection until one is clicked. Until then, the game cannot start.
-var asked = false;
 function askChoice(){
   //alert("in function ask choice");
-  console.log("in ask Choice");
+  if(!asked){
   selectorX.style.textShadow = "4px 4px 4px black";
   selectorX.style.fontSize = "1.5em";
   selectorO.style.textShadow = "4px 4px 4px black";
   selectorO.style.fontSize = "1.5em";
   asked = true;
+  }
 }
 
 //This is called when onclick X or O
 function choosePlayer(choice){
 if(!inPlay && asked){
-  console.log("in Choose Player");
   if(String(choice) == "X"){
    human.side = "X";ai.side = "0";
    selectorX.style.textShadow = "4px 4px 4px black";
@@ -118,15 +140,15 @@ if(!inPlay && asked){
    selectorX.style.fontSize = "1em";}
  }
  inPlay = true;
- console.log("humanside",human.side);
- console.log("aiside",ai.side);
- console.log("inPlay",inPlay);
+ console.log("humansideafterchoose",human.side);
+ console.log("aisideafterchoose",ai.side);
 }
 
 function changeTurn(){
  if(turn === "human"){turn = "ai";}
  else{turn = "human";}
 }
+
 //Start the game
 function startGame(){
  //alert("in here");
@@ -138,16 +160,26 @@ turn = "human";
 
 //destructor function
 function restart(){
+  var domarr = Object.values(parseMap);
+  for (var b = 0; b < domarr.length; b++){
+   eval(domarr[b]).innerHTML = "";
+  }
   inPlay = false;
-  human = new player();
-  ai = new player();
-  turn = "";
-  boardConstraints = ORIGboard;
   asked = false;
+  turn = "";
+  prevAssignment = "";
+  human.side = "";
+  human.solutionStates = ORIGsolutions;
+  ai.side = "";
+  ai.solutionStates = ORIGsolutions;
+  boardConstraints = ORIGneighbors;
+  board = ORIGboard;
   selectorX.style.textShadow = "2px 2px 2px #8F8F8F";
   selectorX.style.fontSize = "1em";
   selectorO.style.textShadow = "2px 2px 2px #8F8F8F";
   selectorO.style.fontSize = "1em";
+  console.log("humanafterrefresh",human);
+  console.log("aiafterrefresh",ai);
 }
 
 //go through the solutions list, and see if any of them have just one left
@@ -163,35 +195,40 @@ function blockOpponent(bbox){
   
 //Check if you may need getters and steters in this context.
 function updateSolutionStates(ssquare){
- var ssquarenum = parseInt(parseMap(ssquare));
+ var ssquarenum = parseInt(ssquare);
  var playersolutions = human.solutionStates;
  var aisolutions = ai.solutionStates;
  for(var m = 0; m < SOLUTION_LENGTH; m++){
    if(playersolutions[m].indexOf(ssquarenum) != -1){
-     human.solutionStates = playersolutions[m][playersolutions[m].indexOf(ssquarenum)] = human.side;
+     playersolutions[m][playersolutions[m].indexOf(ssquarenum)] = human.side;
    }
    if(aisolutions[m].indexOf(ssquarenum) != -1){
-     ai.solutionStates = aisolutions[m][aisolutions[m].indexOf(ssquarenum)] = ai.side;
+     aisolutions[m][aisolutions[m].indexOf(ssquarenum)] = ai.side;
    }
+  human.solutionsStates = playersolutions;
+  ai.solutionStates = aisolutions;
+  console.log("human states",human.solutionStates);
+  console.log("ai states",ai.solutionStates);
  }//end of for loop through solutions;
 }//end of update solution state function
 
 
+//Do this function over again.
 function updateConstraints(csquare){
+ console.log("Board Constraints",boardConstraints);
  for (var key in boardConstraints) {
-    if (boardConstraints.hasOwnProperty(key)) {
       for(var k = 0; k < boardConstraints[key].length; k++){
         if(boardConstraints[key][k] === parseInt(csquare)){
           if(turn === "human"){
-            boardConstraints[key] === human.side;
+            boardConstraints[key] = human.side;
           }//end of if human
           else{
-            boardConstraints[key] === ai.side;
+            boardConstraints[key] = ai.side;
           }///end of if ai
         }//end of if statement for thev value.
       }//end of for loop checking the entre board
-    }//redundantly check if the key is in the map
   }//iterae through the keys of the map
+ console.log("constraints",boardConstraints);
 }//end of the updateConstriants function.
 
 
