4a1cb3ab7c32511ca88471223c69e677dede9774
greg-han <ghan3@mail.ccsf.edu>
2018-05-18 12:32:15 -0700

    Fixed issues related to javascript reference assignments. Now have full 2 player functionality. Need to code ai

--- js/index.js
+++ js/index.js
@@ -4,19 +4,19 @@ window.onload = init;
 //This can be set with "X" and "O" in the slots if need be.
 var selectorX = document.getElementById("X");
 var selectorO = document.getElementById("O");
-
+var winner = document.getElementById("winstate");
 //Framework for the game.
 //previous assignment should not be turn.
 var turn = "";
 var prevAssignment = "";
 var BOARD_LENGTH = 9;
-var SOLUTION_LENGTH = 7;
+var SOLUTION_LENGTH = 8;
 var inPlay = false;
 var asked = false;
 var board = [11,12,13,21,22,23,31,32,33];
-var ORIGboard = [11,12,13,21,22,23,31,32,33];
-var ORIGsolutions = [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33]];
-var ORIGneighbors = {
+var ORIGboard = Object.freeze([11,12,13,21,22,23,31,32,33]);
+var ORIGsolutions = Object.freeze([[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33],[13,22,31],[13,22,31]]);
+var ORIGneighbors = Object.freeze({
  "11" : [11,12,13,21,31,22,33],
  "12" : [11,13,21,22,23],
  "13" : [12,11,23,33,22,31],
@@ -26,7 +26,7 @@ var ORIGneighbors = {
  "31" : [21,11,32,33,22,13],
  "32" : [22,12,31,33,21,23],
  "33" : [32,31,23,13,22,11]
- }
+ });
 
 //End framework
 
@@ -42,24 +42,16 @@ var parseMap = {
  }
 
 //end constructor variables.
-
-//player object.
-//check if you are close to a solution state by seeing if any have one left.
-/*var player = function(){
-  self.side = "";
-  self.solved = false;
-  self.solutionStates = [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33]];
-}*/
 var human = {
   'side' : "",
   'solved' : false,
-  'solutionStates' : [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33]]
+  'solutionStates' : [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33],[13,22,31]]
 }
 
 var ai = {
   'side' : "",
   'solved' : false,
-  'solutionStates' : [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33]]
+  'solutionStates' : [[11,12,13],[11,21,31],[11,22,33],[12,22,32],[21,22,23],[31,32,33],[13,23,33],[13,22,31]]
 }
 
 //constructor
@@ -74,7 +66,7 @@ function init(){
  threeone = document.getElementById("31");
  threetwo = document.getElementById("32");
  threethree = document.getElementById("33");
- boardConstraints = ORIGneighbors;
+ boardConstraints = Object.assign({},ORIGneighbors);
  
 }
 
@@ -82,18 +74,24 @@ function init(){
 
 //Right now, the squares get tagged fro some reason.
 function fillSquare(square){
- console.log("board in fill square",board);
  if (inPlay){
+  //checkForSolution();
+  console.log("Turn",turn);
   var squarenum = parseInt(square);
     //This already checks the constraints.
      //update board.
-     if(turn == "human"){
+     if(turn == 'human'){
+       console.log("I'M IN HERE");
        ///do consistency checks here, do not change turns until
        //a square has been picked. You can
         if(board.indexOf(squarenum) != -1 && prevAssignment != eval(turn).side){
+         console.log("I'M ALSO IN HERE");
          board[board.indexOf(squarenum)] = String(human.side);
          eval(parseMap[String(square)]).innerHTML = human.side;
          prevAssignment = human.side;
+         updateConstraints(squarenum); 
+         updateSolutionStates(squarenum);
+         checkForSolution();
          changeTurn();
         }
 
@@ -103,14 +101,13 @@ function fillSquare(square){
          board[board.indexOf(squarenum)] = String(ai.side);
          eval(parseMap[String(square)]).innerHTML = ai.side;
          prevAssignment = ai.side;
+         updateConstraints(squarenum); 
+         updateSolutionStates(squarenum);
+         checkForSolution();
          changeTurn();
        }//end of if the board is not already filled.
      }//end of else ai
    }//end of inplay
-  //constraint filling function
-  updateConstraints(squarenum); 
-  //update1solution states
-  updateSolutionStates(squarenum);
  }//end of fillsquare 
 
 
@@ -150,19 +147,16 @@ function changeTurn(){
 
 //Start the game
 function startGame(){
+clearBoard();
 if(!inPlay){
   askChoice();
  }
+winner.innerHTML = "";
 turn = "human";
 }
 
-//destructor function
-//javascript is getting the addresses mixed up between board and ORIGboard after aw hile.
-function restart(){
-  //When set as board = ORIGboard, after a few iterations it would fail
-  //so this function stopped that problem for some reason. (Liekly to do with memory addresses.)
-  board = [];
-  ORIGboard.forEach(function(element){
+function clearBoard(){
+   ORIGboard.forEach(function(element){
    board.push(element);
   });
   var domarr = [];
@@ -170,55 +164,95 @@ function restart(){
   for (var b = 0; b < domarr.length; b++){
    eval(domarr[b]).innerHTML = "";
   }
+}
+//destructor function
+//javascript is getting the addresses mixed up between board and ORIGboard after aw hile.
+function restart(){
+  //When set as board = ORIGboard, after a few iterations it would fail
+  //so this function stopped that problem for some reason. (Liekly to do with memory addresses.)
+  board = [];
+  boardConstraints = Object.assign({},ORIGneighbors);
   inPlay = false;
   asked = false;
   turn = "";
   prevAssignment = "";
   human.side = "";
-  human.solutionStates = ORIGsolutions;
+  human.solutionStates = JSON.parse(JSON.stringify(ORIGsolutions));
   ai.side = "";
-  ai.solutionStates = ORIGsolutions;
-  boardConstraints = ORIGneighbors;
+  ai.solutionStates = JSON.parse(JSON.stringify(ORIGsolutions));
   selectorX.style.textShadow = "2px 2px 2px #8F8F8F";
   selectorX.style.fontSize = "1em";
   selectorO.style.textShadow = "2px 2px 2px #8F8F8F";
   selectorO.style.fontSize = "1em";
+  humanWon = [];
+  aiWon = [];
+  hwon = false;
+  awon = false;
+  allFilled = false;
+  draw = false;
 }
 
-//go through the solutions list, and see if any of them have just one left
-//if so, solve!
-function checkForSolution(sbox){
-  
+//go through the solutions list, and see if any of them are solved.
+function checkForSolution(){
+ var humanWon = [];
+ var aiWon = [];
+ var hwon = false;
+ var awon = false;
+ var allFilled = false;
+ var draw = false;
+ human.solutionStates.forEach(function(solutionarray){
+   humanWon.push(
+   solutionarray.every(function(helement){return helement === human.side }));
+ });
+ ai.solutionStates.forEach(function(solutionarray){
+    aiWon.push(
+   solutionarray.every(function(aelement){return aelement === ai.side }));
+ });
+ if(humanWon.indexOf(true) != -1){hwon = true;}
+ if(aiWon.indexOf(true) != -1){awon = true;}
+ allFilled = board.every(function(be){return typeof be === "string" });
+ if(!hwon && !awon && allFilled){
+  draw = true;
+ }
+ if(draw){winner.innerHTML = "DRAW"; restart();}
+ if(hwon){winner.innerHTML = "YOU WON"; restart();}
+ if(awon){winner.innerHTML = "YOU LOST"; restart();}
+ humanWon = [];
+ aiWon = [];
+ hwon = false;
+ awon = false;
+ allFilled = false;
+ draw = false;
 }
 
-function blockOpponent(bbox){
+function blockOrSolve(bbox){
  //go through opponents solutions list, and check if any of them have just one left.
  //if so, block
+ //go through your own solutions list, and see if any have just one solution left if so, solve
 }
   
 //Check if you may need getters and steters in this context.
+
 function updateSolutionStates(ssquare){
  var ssquarenum = parseInt(ssquare);
- var playersolutions = human.solutionStates;
- var aisolutions = ai.solutionStates;
  for(var m = 0; m < SOLUTION_LENGTH; m++){
-   if(playersolutions[m].indexOf(ssquarenum) != -1){
-     playersolutions[m][playersolutions[m].indexOf(ssquarenum)] = human.side;
+   if((human.solutionStates[m].indexOf(ssquarenum) != -1) && turn == "human"){
+     human.solutionStates[m][human.solutionStates[m].indexOf(ssquarenum)] = human.side;
+     console.log("human square num",ssquarenum);
+     console.log("humansolutions",human.solutionStates);
    }
-   if(aisolutions[m].indexOf(ssquarenum) != -1){
-     aisolutions[m][aisolutions[m].indexOf(ssquarenum)] = ai.side;
+   if((ai.solutionStates[m].indexOf(ssquarenum) != -1) && turn == "ai"){
+     ai.solutionStates[m][ai.solutionStates[m].indexOf(ssquarenum)] = ai.side;
+     console.log("aisolutions",ai.solutionStates);
    }
-  human.solutionsStates = playersolutions;
-  ai.solutionStates = aisolutions;
  }//end of for loop through solutions;
-  //console.log("human states",human.solutionStates);
-  //console.log("ai states",ai.solutionStates);
+  console.log("human states",human.solutionStates);
+  console.log("ai states",ai.solutionStates);
 }//end of update solution state function
 
 
 //Do this function over again.
 function updateConstraints(csquare){
- console.log("Board Constraints B4",boardConstraints);
  var constraintarrs = Object.values(boardConstraints);
  var keyarrs = Object.keys(boardConstraints);
  for (var n = 0; n < constraintarrs.length; n++) {
