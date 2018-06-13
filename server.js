55dfe0b235c9aefcd152b64071d1a21304982f16
greg-han <ghan3@mail.ccsf.edu>
2018-06-13 12:10:46 -0700

    This actually works now! added express/node server to host on heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname)));
+app.use("/styles",express.static(__dirname + '/styles'));
+app.use("/images", express.static(__dirname + '/images'));
+app.use("/scripts", express.static(__dirname + '/scripts'));
+
+app.get('/',function (req, res){
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
+
